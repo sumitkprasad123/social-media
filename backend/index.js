c77399a362dde464637b73f54d32d0eae02413ce
sumit <sumitkprasad123@gmail.com>
2023-10-30 21:21:55 +0530

    social-media

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const dotenv = require("dotenv")
+const helmet = require("helmet")
+const morgan = require("morgan")
+const mongoose = require('mongoose');
+const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
+const postRoute = require("./routes/posts")
+
+const app = express()
+dotenv.config()
+
+
+// middleware
+app.use(express.json())
+app.use(helmet());
+app.use(morgan("common"))
+
+app.use("/api/users", userRoute)
+app.use("/api/auth", authRoute)
+app.use("/api/posts", postRoute)
+
+app.get("/",(req,res) => {
+     res.send("welcome to homepage")
+})
+
+app.listen(process.env.PORT,async() => {
+   try{
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log("server is connected to mongodb")
+   }catch(err){
+        console.log("Can not connected to mongodb")
+        console.log(err)
+   }
+    console.log(`server is running on port ${process.env.PORT}`)
+})

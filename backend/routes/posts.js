a72da2f39b99f642a2c2dc95a72a31208e3bb2c4
sumit <sumitkprasad123@gmail.com>
2023-12-21 10:40:33 +0530

    delete posts

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -37,14 +37,14 @@ router.put("/:id",async(req,res) => {
    try{
       //   const post = await Post.findById(req.params.id);
       //   if(post.userId === req.body.userId){
-        if(req.body.postId){
-          console.log({"p":req.body.postId})
+      //   if(req.body.postId){
+      //     console.log({"p":req.body.postId})
          //   await post.deleteOne();
-           await Post.findByIdAndDelete(req.body.postId.toString())
+           await Post.findByIdAndDelete(req.body.postId)
            res.status(200).json("post has been deleted")
-        }else{
-           res.status(403).json("you can delete only your post")
-        }
+      //   }else{
+      //      res.status(403).json("you can delete only your post")
+      //   }
    }catch(err){
         res.status(500).json(err)
    }
